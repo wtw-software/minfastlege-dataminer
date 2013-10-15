7a8f2ec00e4e5145b274b54e1ae061fc02f01cdf
andtan <jornandretangen@gmail.com>
2013-10-15 10:25:15 +0200

    spaces as tabs

--- tests/kommune.js
+++ tests/kommune.js
@@ -1,17 +1,17 @@
 var expect = require( 'expect.js' ),
-		kommune = require( '../lib/kommune' )
+    kommune = require( '../lib/kommune' )
 
 describe( "kommune", function(){
 
-	describe( "kommune.getAllByFylkeId", function() {
+  describe( "kommune.getAllByFylkeId", function() {
 
-		it("should provide an array with objects konsisiting of id and name", function( done ) {
-			kommune.getAllByFylkeId("04", function( err, kommuner ) {
-				console.log( kommuner)
-				done()
-			})
-		})
+    it("should provide an array with objects konsisiting of id and name", function( done ) {
+      kommune.getAllByFylkeId("04", function( err, kommuner ) {
+        console.log( kommuner)
+        done()
+      })
+    })
 
-	})
+  })
 
 })
